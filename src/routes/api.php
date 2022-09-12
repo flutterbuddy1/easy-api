aff01fcaf475ddf081a199d9bce359eed5f9479f
flutterbuddy1 <mayanksmind@gmail.com>
2022-09-13 00:52:07 +0530

    Upgraded

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('{table}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'index']);
+Route::get('easy/{table}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'index']);
     
-Route::get('{table}/{id}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'get']);
+Route::get('easy/{table}/{id}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'get']);
 
-Route::post('{table}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'post']);
+Route::post('easy/{table}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'post']);
 
-Route::put('update/{table}/{id}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'update']);
+Route::put('easy/update/{table}/{id}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'update']);
 
-Route::delete('delete/{table}/{id}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'delete']);
+Route::delete('easy/delete/{table}/{id}',[\Flutterbuddy1\EasyApi\Http\Controllers\ApiController::class,'delete']);
